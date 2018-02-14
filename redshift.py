c4c7de01a61d7a1048b2799249d6474c7f76200f
sumitverma1! <somiverma@gmail.com>
2018-02-14 08:33:15 +0530

    bigdata assignment

---
+++ redshift.py
@@ -0,0 +1,25 @@
+import re
+import numpy as np
+import pandas
+
+file1=open('nr.txt')
+
+file2=file1.readlines()
+
+result1 = [k for k in file2 if '--' not in k]
+
+result2 = [k for k in result1 if '\\' not in k]
+
+result3=re.sub("WITH SYSID\s\d+","",str(result2))
+
+result4=result3.split(",")
+
+result5 = [k for k in result4 if '[' not in k] 
+result6 = [k for k in result5 if ']' not in k] 
+
+'''values=[]
+for line in result4:
+    values.append(line)'''
+
+
+values = np.asarray(result6)
